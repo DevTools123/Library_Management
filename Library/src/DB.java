f4f18b2012284822d925103d66dc181feceada47
VishwasNM <vishwas07.vnm@gmail.com>
2019-04-02 16:01:53 +0530

    ITP-8 #comment Updated the same #In-Progress

--- Library/src/DB.java
+++ Library/src/DB.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DB {
 	public static Connection getConnection(){
 		Connection con=null;
-		try{
+		try{ //updated try catch function
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","");
 		}catch(Exception e){System.out.println(e);}
